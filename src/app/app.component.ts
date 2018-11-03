5143aa9f5b800e346c07411ad55261f01c952133
Gan San Yong <sygan3@gmail.com>
2018-11-04 00:37:08 +0800

    add message services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MessagesService } from '../app/services/messages.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'airquality';
+  constructor(public messageService: MessagesService) { 
+    this.messageService.add('Message: Success~');
+    this.messageService.add('Message: ~1~');
+    this.messageService.add('Message: ~2~');
+    this.messageService.add('Message: ~3~');
+    this.messageService.add('Message: LATEST');
+
+  }
 }
